90a4cba1e008e783461a83f472c0200e417adaac
LamKamhang <cool_lam@outlook.com>
2019-05-30 14:08:31 +0800

    update font

--- init.el
+++ init.el
@@ -49,6 +49,7 @@
  '(company-minimum-prefix-length 2)
  '(counsel-bookmark-avoid-dired nil)
  '(custom-enabled-themes (quote (tsdh-dark)))
+ '(display-time-mode t)
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
@@ -80,12 +81,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "outline" :slant normal :weight bold :height 158 :width normal))))
  '(swiper-line-face ((t (:inherit highlight)))))
 
 (if (eq window-system 'w32)
     (custom-set-faces
-     '(default ((t (:family "SimHei" :foundry "outline" :slant normal :weight bold :height 181 :width normal)))))
+     '(default ((t (:family "SimHei" :foundry "outline" :slant normal :weight normal :height 181 :width normal)))))
   (custom-set-faces
    '(default ((t (:family "Ubuntu Mono" :foundry "outline" :slant normal :weight bold :height 158 :width normal))))))
 
