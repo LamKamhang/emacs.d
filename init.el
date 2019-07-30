a6892323432a77a11967a58393d88134b981cac4
jakimlin <jakimlin@tencent.com>
2019-07-30 09:40:39 +0800

    update net config

--- init.el
+++ init.el
@@ -220,6 +220,35 @@
 (global-set-key (kbd "C-o") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
+;; Configure network proxy
+(setq my-proxy "web-proxy.tencent.com:8080")
+(defun show-proxy ()
+  "Show http/https proxy."
+  (interactive)
+  (if url-proxy-services
+      (message "Current proxy is \"%s\"" my-proxy)
+    (message "No proxy")))
+
+(defun set-proxy ()
+  "Set http/https proxy."
+  (interactive)
+  (setq url-proxy-services `(("http" . ,my-proxy)
+                             ("https" . ,my-proxy)))
+  (show-proxy))
+
+(defun unset-proxy ()
+  "Unset http/https proxy."
+  (interactive)
+  (setq url-proxy-services nil)
+  (show-proxy))
+
+(defun toggle-proxy ()
+  "Toggle http/https proxy."
+  (interactive)
+  (if url-proxy-services
+      (unset-proxy)
+    (set-proxy)))
+
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
