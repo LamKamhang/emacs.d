899fc3efa8ab72011570ae6c7938c38192344479
LamKamhang <Cool_Lam@outlook.com>
2019-11-02 20:10:25 +0800

    update emacs config

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -4,8 +4,8 @@
 
 (require 'init-frame-hooks)
 
-(global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
-(global-set-key [mouse-5] (lambda () (interactive) (scroll-up 1)))
+(global-set-key [mouse-4] (lambda () (interactive) (scroll-down 8)))
+(global-set-key [mouse-5] (lambda () (interactive) (scroll-up 8)))
 
 (autoload 'mwheel-install "mwheel")
 
