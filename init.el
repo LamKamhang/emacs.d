4a43ee4efb70a2943a6a3b8a1236023f9ed7103b
LamKamhang <Cool_Lam@outlook.com>
2019-11-06 23:32:14 +0800

    remove server config

--- init.el
+++ init.el
@@ -218,11 +218,11 @@
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
 ;;----------------------------------------------------------------------------
-(add-hook 'after-init-hook
-          (lambda ()
-            (require 'server)
-            (unless (server-running-p)
-              (server-start))))
+;; (add-hook 'after-init-hook
+;;           (lambda ()
+;;             (require 'server)
+;;             (unless (server-running-p)
+;;               (server-start))))
 
 ;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
