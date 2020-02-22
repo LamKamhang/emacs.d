27dde2f065755beeae0c96d716000b0d202a6950
LamKamhang <lamkamh@zju.edu.cn>
2020-02-22 14:50:04 +0800

    change C-s from swiper to counsel-grep-or-swiper

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -79,7 +79,8 @@ instead."
   (setq enable-recursive-minibuffers t)
   ;; enable this if you want `swiper' to use it
   ;; (setq search-default-mode #'char-fold-to-regexp)
-  (global-set-key (kbd "C-s") 'swiper)
+  ;; (global-set-key (kbd "C-s") 'swiper)
+  (global-set-key (kbd "C-s") 'counsel-grep-or-swiper)
   (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "M-y") 'counsel-yank-pop)
