88cd0804fe7e7529b916fb33569acb141f5d9874
LamKamhang <lamkamh@zju.edu.cn>
2020-03-10 19:48:16 +0800

    fix yasnippet reload bug

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -2,15 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
-;; my private snippets, should be placed before enabling yasnippet
-(require-package 'yasnippet)
-(add-hook 'prog-mode-hook #'yas-minor-mode)
-(add-hook 'text-mode-hook #'yas-minor-mode)
-;; {{ modes do NOT inherit from prog-mode
-(add-hook 'cmake-mode-hook #'yas-minor-mode)
-(add-hook 'web-mode-hook #'yas-minor-mode)
-(add-hook 'scss-mode-hook #'yas-minor-mode)
-;; }}
+(when (require-package 'yasnippet)
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  (add-hook 'text-mode-hook #'yas-minor-mode)
+  ;; {{ modes do NOT inherit from prog-mode
+  (add-hook 'cmake-mode-hook #'yas-minor-mode)
+  (add-hook 'web-mode-hook #'yas-minor-mode)
+  (add-hook 'scss-mode-hook #'yas-minor-mode)
+  ;; }}
+  (require-package 'yasnippet-snippets)
+
+  (when (maybe-require-package 'ivy-yasnippet)
+    (global-set-key (kbd "C-c i") 'ivy-yasnippet))
+
+  (after-load 'yasnippet
+    (yas-reload-all)))
 
 (defun my-yas-reload-all ()
   "Compile and reload yasnippets.  Run the command after adding new snippets."
@@ -18,11 +24,6 @@
   (yas-compile-directory (file-truename "~/.emacs.d/snippets"))
   (yas-reload-all)
   (yas-minor-mode 1))
-
-(require-package 'yasnippet-snippets)
-
-(when (maybe-require-package 'ivy-yasnippet)
-  (global-set-key (kbd "C-c i") 'ivy-yasnippet))
 
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
