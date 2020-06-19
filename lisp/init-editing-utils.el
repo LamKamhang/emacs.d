c3fa3836a456cbc491a99c9c25e02e4aba73a200
LamKamhang <lamkamh@zju.edu.cn>
2020-06-19 21:12:17 +0800

    better usage of avy

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -171,7 +171,8 @@
 (global-set-key (kbd "C-x C-.") 'pop-global-mark)
 
 (when (maybe-require-package 'avy)
-  (global-set-key (kbd "C-;") 'avy-goto-char-timer))
+  (global-set-key (kbd "C-;") 'avy-goto-char-timer)
+  (global-set-key (kbd "M-g l") 'avy-goto-line))
 
 (require-package 'multiple-cursors)
 ;; multiple-cursors
