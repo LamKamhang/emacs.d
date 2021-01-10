e83072d31688fe1dfd2707855f7763c2daaaa85b
LamKamhang <lamkamh@zju.edu.cn>
2021-01-10 14:47:09 +0800

    add more ext for glsl

--- lisp/init-glsl.el
+++ lisp/init-glsl.el
@@ -10,5 +10,12 @@
 (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.comp\\'" . glsl-mode))
 
+(add-to-list 'auto-mode-alist '("\\.vs\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.fs\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.tcs\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.tes\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.gs\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.cs\\'" . glsl-mode))
+
 (provide 'init-glsl)
 ;;; init-glsl.el ends here
