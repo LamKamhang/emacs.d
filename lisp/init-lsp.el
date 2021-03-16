e1ab41dc22333bc08099d3b12e24357240ed861d
LamKamhang <lamkamh@zju.edu.cn>
2021-03-16 14:55:45 +0800

    do not use dap mode

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -37,10 +37,23 @@
   :ensure t
   :commands lsp-treemacs-errors-list)
 
-;; optionally if you want to use debugger
-(use-package dap-mode
-  :ensure t)
-(require 'dap-cpptools)
+;; ;; optionally if you want to use debugger
+;; (use-package dap-mode
+;;   :ensure t)
+;; (require 'dap-cpptools)
+;; (dap-mode 1)
+
+;; ;; The modes below are optional
+
+;; (dap-ui-mode 1)
+;; ;; enables mouse hover support
+;; (dap-tooltip-mode 1)
+;; ;; use tooltips for mouse hover
+;; ;; if it is not enabled `dap-mode' will use the minibuffer.
+;; (tooltip-mode 1)
+;; ;; displays floating panel with debug buttons
+;; ;; requies emacs 26+
+;; (dap-ui-controls-mode 1)
 
 (use-package ivy-xref
   :ensure t
