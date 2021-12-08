ac1ceea5a588ea1b2f348d59fd063ddcf5c39056
LamKamhang <lamkamh@zju.edu.cn>
2021-12-08 18:54:04 +0800

    update my-cpp-highlight

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -36,6 +36,19 @@
                 *my-cpp-include-path*))
 ;; (with-eval-after-load 'company
 ;;   (add-to-list 'company-backends `(company-capf company-c-headers)))
+(defun my-cpp-highlight ()
+  "Highlight c/c++ #if 0 #endif macros."
+  ;; (interactive)
+  (setq cpp-known-face 'default)
+  (setq cpp-unknown-face 'default)
+  (setq cpp-known-writable 't)
+  (setq cpp-unknown-writable 't)
+  (setq cpp-edit-list '(("0" font-lock-comment-face default both)
+                        ("1" default font-lock-comment-face both)))
+  (cpp-highlight-buffer t))
+
+(add-hook 'c-mode-common-hook 'my-cpp-highlight)
+(add-hook 'after-save-hook 'my-cpp-highlight)
 
 (provide 'init-cc)
 ;;; init-cc.el ends here
